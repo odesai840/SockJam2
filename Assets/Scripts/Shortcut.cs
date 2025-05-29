e46e04432f74651f7f70f6acf0325499306c874c
Caleb Kronstad <calebtkronstad@gmail.com>
2025-05-29 16:12:10 -0400

    shortcuts

---
+++ Assets/Scripts/Shortcut.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Shortcut : MonoBehaviour
+{
+    public BoxCollider2D blockage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            blockage.enabled = false;
+            blockage.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
